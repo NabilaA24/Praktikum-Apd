66615a8b0880cebc644b3a8f2003a45a58e30874
NabilaA24 <nabilataka0@gmail.com>
2024-09-16 21:21:02 +0800

    Add files via upload

---
+++ Praktikum-APD/Post-Test/Post-Test-2/2409106036_Nabila_A2'24_posttest(2).py
@@ -0,0 +1,24 @@
+nama = input("masukkan nama :" )
+NIM = input("masukkan NIM :")
+harga_beras = int(input("masukkan harga beras :"))
+
+Persentase_Diskon_mawar = 0.11
+Persentase_Diskon_sania = 0.14
+Persentase_Diskon_maknyus = 0.17
+
+Diskon_Beras_Mawar = harga_beras*Persentase_Diskon_mawar
+Diskon_Beras_Sania = harga_beras*Persentase_Diskon_sania
+Diskon_Beras_Maknyus = harga_beras*Persentase_Diskon_maknyus
+
+Harga_Diskon_Beras_Mawar = harga_beras - Diskon_Beras_Mawar
+Harga_Diskon_Beras_Sania = harga_beras - Diskon_Beras_Sania
+Harga_Diskon_Beras_Maknyus = harga_beras - Diskon_Beras_Maknyus
+
+ouput = (
+    nama + " dengan NIM " + NIM + " ingin membeli beras seharga Rp" + str(harga_beras) +"\n"
+    "Jika dia membeli beras Mawar ia harus membayar " + str(int(Harga_Diskon_Beras_Mawar)) + " Setelah mendapat diskon 11%.\n" 
+    "Jika dia membeli beras Sania ia harus membayar " + str(int(Harga_Diskon_Beras_Sania)) + " Setelah mendapat diskon 14%.\n"
+    "Jika dia membeli beras Maknyus ia harus membayar " + str(int(Harga_Diskon_Beras_Maknyus)) + " Setelah mendapat diskon 17%."
+    )
+
+print(ouput)
